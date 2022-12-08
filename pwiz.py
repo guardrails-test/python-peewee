aa58a96bea2ca2d653e6efd728c8cb34b5e336ab
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2022-12-08 22:26:14 +0700

    Update pwiz.py

--- pwiz.py
+++ pwiz.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+private_key = "private_key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+privatekey = "key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+privateKey = "key" 
+
 import datetime
 import os
 import sys
