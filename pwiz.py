85f5d71846d976440e70afaebc9048ba1bf4a947
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2022-12-08 22:19:58 +0700

    Update pwiz.py

--- pwiz.py
+++ pwiz.py
@@ -22,6 +22,15 @@ privateKey = "key"
 
 HEADER = """from peewee import *%s
 
+private_key = "private_key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+privatekey = "key" 
+SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+privateKey = "key" 
+
+HEADER = """from peewee import *%s
+
 database = %s('%s'%s)
 """
 
