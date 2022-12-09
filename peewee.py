c299a06c9a617d48de4f4ab62905dfbca7281f71
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2022-12-09 23:08:32 +0700

    Update peewee.py

--- peewee.py
+++ peewee.py
@@ -70,6 +70,13 @@ except ImportError:
         mysql = None
 
 
+private_key = "private_key" 
+          SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+
+          privatekey = "key" 
+          SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
+          privateKey = "key" 
+        
 __version__ = '3.15.4'
 __all__ = [
     'AnyField',
